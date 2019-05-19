5cc20656d4fe620423ed8282103d2ed128fb1476
fggo <jnuho@outlook.com>
2019-05-19 23:29:13 +0900

    password hash algorithm

--- lotto/src/com/kh/lotto/controller/UserController.java
+++ lotto/src/com/kh/lotto/controller/UserController.java
@@ -1,23 +1,81 @@
 package com.kh.lotto.controller;
 
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 import com.kh.lotto.model.vo.User;
 import com.kh.lotto.view.MainMenu;
 
 public class UserController {
-//	private final static int MAX_CNT = 25;
-//	private User[] users = new User[MAX_CNT];
 	private HashSet<User> users = new HashSet<User>();
 
 	public void mainMenu() {
 		MainMenu.mainMenu();
 	}
 	
-	public void input() {
-		User user = MainMenu.inputView();
+	public void signUp() {
+		Map<String, Object> hmap = MainMenu.signUpView();
+		String originalPassword = (String)hmap.get("password");
+		String strongPassword = "";
+
+		try {
+			//hash password
+			strongPassword = UserController.generateStrongPasswordHash(originalPassword);
+		}
+		catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		catch(InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+
+		User user = new User((String)hmap.get("username"),
+							 strongPassword,
+							 (char)hmap.get("grade"),
+							 (LocalDateTime)hmap.get("createdDate"),
+							 (String)hmap.get("accountNumber"),
+							 (int)hmap.get("userPoint"),
+							 (int)hmap.get("userMoney"),
+							 (HashSet<String>)hmap.get("gambleRecord"));
 		users.add(user);
+	}
+
+	public void signIn() {
+		Map<String, String> map = MainMenu.signInView();
+		String username = map.get("username");
+		User user = search(username);
+		if(user == null) {
+			System.out.println("로그인 정보가 맞지 않습니다.");
+			return;
+		}
+		String originalPassword = map.get("password");
+		String storedPassword = user.getPassword();
+
+		boolean loginSuccess = false;
+
+		try {
+			loginSuccess = validatePassword(originalPassword, storedPassword);
+		}
+		catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		catch(InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+
+		if(loginSuccess)
+			System.out.println("로그인이 성공적으로 되었습니다!");
+		else
+			System.out.println("로그인이 실패했습니다...");
 	}
 
 	public void modify() {
@@ -47,6 +105,17 @@ public class UserController {
 			//password
 			System.out.print("비밀번호 : ");
 			String password = MainMenu.CONSOLE.nextLine();
+			try {
+				//hash password
+				password = UserController.generateStrongPasswordHash(password);
+			}
+			catch(NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			catch(InvalidKeySpecException e) {
+				e.printStackTrace();
+			}
+
 			//accountNumber
 			System.out.print("계좌번호 : ");
 			String accountNumber = MainMenu.CONSOLE.nextLine();
@@ -115,6 +184,76 @@ public class UserController {
 		}
 		return null;
 	}
+	
+/* 비밀번호를 Hash알고리즘으로 암호화하기
+ * PBKDF2WithHmacSHA1 알고리즘 사용
+ * 참고사이트:
+ * https://howtodoinjava.com/security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+ */
+	private static String generateStrongPasswordHash(String password) 
+			throws NoSuchAlgorithmException, InvalidKeySpecException{
+		int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
+         
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+	}
 
+	private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+     
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
+    }
+    
+/* 비밀번호 일치여부 확인
+ * 참고사이트:
+ * https://howtodoinjava.com/security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+ */
+    private static boolean validatePassword(String originalPassword, String storedPassword) 
+    		throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        String[] parts = storedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+         
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+         
+        int diff = hash.length ^ testHash.length;
+        for(int i = 0; i < hash.length && i < testHash.length; i++)
+        {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
+
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+    {
+        byte[] bytes = new byte[hex.length() / 2];
+        for(int i = 0; i<bytes.length ;i++)
+        {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+    
 	public HashSet<User> getUsers() { return users; }
 }
--- lotto/src/com/kh/lotto/view/MainMenu.java
+++ lotto/src/com/kh/lotto/view/MainMenu.java
@@ -1,15 +1,16 @@
 package com.kh.lotto.view;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 
 import com.kh.lotto.controller.UserController;
 import com.kh.lotto.main.Main;
-import com.kh.lotto.model.vo.User;
 
 interface INPUT_SELECT{
-	int INPUT=1, MODIFY=2, DELETE=3, SEARCH=4, SEARCHALL=5, EXIT=0;
+	int SIGNUP=1, SIGNIN=2, MODIFY=3, DELETE=4, SEARCH=5, SEARCHALL=6, EXIT=0;
 }
 
 interface USER_GRADE{
@@ -25,17 +26,19 @@ public class MainMenu {
 		do {
 			System.out.println("===== 회원 정보관리 프로그램 =====");
 			System.out.println("1. 회원 가입");
-			System.out.println("2. 회원 수정");
-			System.out.println("3. 회원 탈퇴");
-			System.out.println("4. 회원 조회");
-			System.out.println("5. 회원 전체조회");
+			System.out.println("2. 회원 로그인");
+			System.out.println("3. 회원 수정");
+			System.out.println("4. 회원 탈퇴");
+			System.out.println("5. 회원 조회");
+			System.out.println("6. 회원 전체조회");
 			System.out.println("0. 프로그램 종료");
 			System.out.print("메뉴선택 : ");
 
 			choice = CONSOLE.nextInt(); CONSOLE.nextLine();
 
 			switch(choice) {
-				case INPUT_SELECT.INPUT: controller.input(); break;
+				case INPUT_SELECT.SIGNUP: controller.signUp(); break;
+				case INPUT_SELECT.SIGNIN: controller.signIn(); break;
 				case INPUT_SELECT.MODIFY: controller.modify(); break;
 				case INPUT_SELECT.DELETE: controller.delete(); break;
 				case INPUT_SELECT.SEARCH: controller.search(); break;
@@ -47,7 +50,7 @@ public class MainMenu {
 		}while(choice !=9);
 	}
 	
-	public static User inputView() {
+	public static Map<String, Object> signUpView() {
 		System.out.println("===== 회원 가입 =====");
 
 		//username
@@ -75,8 +78,33 @@ public class MainMenu {
 		//gambleRecord
 		HashSet<String> gambleRecord = new HashSet<String>();
 		
-		User user = new User(username, password, grade, createdDate, accountNumber, userPoint, userMoney, gambleRecord);
-		return user;
+		Map<String, Object> hmap = new HashMap<String, Object>();
+		hmap.put("username", username);
+		hmap.put("password", password);
+		hmap.put("grade", Character.valueOf(grade));
+		hmap.put("createdDate", createdDate);
+		hmap.put("accountNumber", accountNumber);
+		hmap.put("userPoint", Integer.valueOf(userPoint));
+		hmap.put("userMoney", Integer.valueOf(userMoney));
+		hmap.put("gambleRecord", gambleRecord);
+		
+		return hmap;
+	}
+	
+	public static Map<String, String> signInView() {
+		System.out.println("===== 회원 로그인 =====");
+		//username
+		System.out.print("로그인 사용자 아이디 : ");
+		String username = CONSOLE.nextLine();
+		//password
+		System.out.print("로그인 사용자 비밀번호 : ");
+		String password = CONSOLE.nextLine();
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", username);
+		map.put("password", password);
+
+		return map;
 	}
 	
 	public static String searchView() {
