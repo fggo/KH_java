a40907124e7e82cbacc4b7630e520c24f4b1ed5c
fggo <jnuho@oultook.com>
2019-06-26 08:01:11 +0900

    .

--- _doc/과제/마지막과제/SQL_Workshop4_Sol.sql
+++ _doc/과제/마지막과제/SQL_Workshop4_Sol.sql
@@ -8,3 +8,13 @@ GROUP BY G.class_no, C.class_name
 order by G.class_no;
 
 --17
+select S1.student_name, S1.student_address
+from tb_student S1 JOIN tb_student S2 USING(department_no)
+where S2.student_name='최경희';
+
+--18
+--select S1.student_no, student_name, G.point
+--from tb_student S1 
+--    JOIN tb_department D USING (department_no)
+--    JOIN tb_grade G ON S1.student_no = G.student_no
+--where department_name='국어국문학과';
