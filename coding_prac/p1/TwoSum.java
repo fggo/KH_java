ce37014bdbb5a140f76762dcd89fd86d47bfcc27
fggo <jnuho@oultook.com>
2019-07-10 01:36:41 +0900

    .

--- coding_prac/p1/TwoSum.java
+++ coding_prac/p1/TwoSum.java
@@ -7,12 +7,25 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target){
         Map<Integer,Integer> map = new HashMap<Integer, Integer>(); 
         for(int i= 0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i]))
+                return new int[]{i, map.get(target-nums[i])};
             map.put(nums[i], i);
-            
         }
 
+        throw new IllegalArgumentException("No two sum solution");
     }
     public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+
+        int []indices = ts.twoSum(new int[]{2, 7, 11, 15}, 9);
+        for(int idx : indices)
+            System.out.print(idx + " ");
 
     }
 }
+
+// keyword { "fontStyle": "bold" }
+// storage { "foreground": "#1857AB" }
+// storage.type { "foreground": "#1857AB" }
+// variable.other { "foreground": "#E7F6DA" }
+// entity.name.function { "fontStyle": "", "foreground": "#E7F6DA" }
