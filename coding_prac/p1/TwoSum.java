e82fef2f0a61d28c8ff609ee87d8fb65ca4c0748
fggo <jnuho@outlook.com>
2019-07-09 15:01:17 +0900

    .

---
+++ coding_prac/p1/TwoSum.java
@@ -0,0 +1,18 @@
+package p1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>(); 
+        for(int i= 0; i<nums.length; i++){
+            map.put(nums[i], i);
+            
+        }
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
