4c6b98a4cff4c828d4bc0f53f624370156261b92
fggo <jnuho@oultook.com>
2019-07-11 00:53:16 +0900

    .

--- coding_prac/P001_TwoSum.java
+++ coding_prac/P001_TwoSum.java
@@ -1,9 +1,7 @@
-package p1;
-
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class P001_TwoSum {
     public int[] twoSum(int[] nums, int target){
         Map<Integer,Integer> map = new HashMap<Integer, Integer>(); 
         for(int i= 0; i<nums.length; i++){
@@ -15,7 +13,7 @@ public class TwoSum {
         throw new IllegalArgumentException("No two sum solution");
     }
     public static void main(String[] args) {
-        TwoSum ts = new TwoSum();
+        P001_TwoSum ts = new P001_TwoSum();
 
         int []indices = ts.twoSum(new int[]{2, 7, 11, 15}, 9);
         for(int idx : indices)
