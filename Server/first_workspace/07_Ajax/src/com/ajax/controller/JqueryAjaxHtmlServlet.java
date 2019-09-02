ace13045afb0ca0c7d03e28694f5499665c813ae
junholee <jnuho@outlook.com>
2019-09-02 15:17:35 +0900

    .

--- Server/first_workspace/07_Ajax/src/com/ajax/controller/JqueryAjaxHtmlServlet.java
+++ Server/first_workspace/07_Ajax/src/com/ajax/controller/JqueryAjaxHtmlServlet.java
@@ -27,7 +27,7 @@ public class JqueryAjaxHtmlServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-  /**
+  /*
    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
